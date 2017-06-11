103d47e5c39f6ba42ee0105ed4ff9942ccd19a5d
Arthur Violy <arthur@violy.net>
2017-06-11 14:33:28 +0200

    socket io port

--- index.js
+++ index.js
@@ -96,11 +96,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-    });
-});
+io.on('connection', function(socket){});
 
 // UDP Client
 const dgram = require('dgram');
